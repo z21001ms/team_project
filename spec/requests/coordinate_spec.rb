88fd13bc1888d470712a919d19e06597513460cb
konny0311 <tatsuya.kono.dev@gmail.com>
2022-01-16 19:58:44 +0900

    add a unit test of SongController

--- spec/requests/coordinate_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Coordinates", type: :request do
-  #TODO Add test
-end
--- spec/requests/song_spec.rb
+++ spec/requests/song_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SongController, type: :controller do
-  # DB使うテストはGithub Actionsでエラーになるので保留
-  # describe "#index" do
-  #   it "登録されている楽曲情報を表示する" do
-  #     get :index
-  #     expect(response).to be_successful
-  #   end
-  # end
+  describe "#index" do
+    it "登録されている楽曲情報を表示する" do
+      get :index
+      expect(response).to be_successful
+    end
+  end
 end
