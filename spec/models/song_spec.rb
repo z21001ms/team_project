c888fd820f7c2ac7eb97f0ab358b787a22b496a4
umiwatarin <kaito.946nct@gmail.com>
2022-01-19 00:47:25 +0900

    Compleate add YouTube Player

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: songs
+#
+#  id         :bigint           not null, primary key
+#  platform   :integer          not null
+#  url        :string(2048)     not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  video_id   :string           not null
+#
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
